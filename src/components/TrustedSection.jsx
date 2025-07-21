487f8405470321994d08bd427afb16de859cc484
vinitchaudhary080 <vinitchaudhary080@gmail.com>
2025-07-22 00:02:40 +0530

    deltalogo

--- src/components/TrustedSection.jsx
+++ src/components/TrustedSection.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import innovioLogo from "../assets/innovio.png";
 import zenzapLogo from "../assets/zenzap.png";
 import sparkleLogo from "../assets/sparkle.png";
+import sparkleLogos from "../assets/deltalogo.png";
 import lumLabsLogo from "../assets/lum-labs.png";
 import pulseLogo from "../assets/pulse.png";
 import craftgramLogo from "../assets/craftgram.png";
@@ -11,10 +12,10 @@ const TrustedSection = () => (
   <section className="py-12 bg-white">
     <div className="container mx-auto text-center px-4">
       <p className="text-sm text-gray-500 mb-6">
-        Trusted by 10,000+ founders &amp; business owners
+        Exclusive Broker Delta Exchange
       </p>
       <div className="flex flex-wrap items-center justify-center gap-8">
-        <img
+        {/* <img
           src={innovioLogo}
           alt="Innovio"
           className="h-8 grayscale hover:grayscale-0 transition duration-300"
@@ -23,13 +24,13 @@ const TrustedSection = () => (
           src={zenzapLogo}
           alt="ZenZap"
           className="h-8 grayscale hover:grayscale-0 transition duration-300"
-        />
+        /> */}
         <img
-          src={sparkleLogo}
+          src={sparkleLogos}
           alt="Sparkle"
-          className="h-8 grayscale hover:grayscale-0 transition duration-300"
+          className="h-20 grayscale hover:grayscale-0 transition duration-300"
         />
-        <img
+        {/* <img
           src={lumLabsLogo}
           alt="Lum Labs"
           className="h-8 grayscale hover:grayscale-0 transition duration-300"
@@ -43,7 +44,7 @@ const TrustedSection = () => (
           src={craftgramLogo}
           alt="Craftgram"
           className="h-8 grayscale hover:grayscale-0 transition duration-300"
-        />
+        /> */}
       </div>
     </div>
   </section>
